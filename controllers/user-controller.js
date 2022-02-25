5217a758ec99c9077a353ae3145f24b8312faf71
Christopher Speltz <chrisspeltz@msn.com>
2022-02-25 17:19:56 -0600

    updated user routes/controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -26,7 +26,7 @@ const userController = {
         .select("-__v")
         .then(dbUserData => {
             if (!dbUserData) {
-                res.status(404).json({ message: "No user found with this id" });
+                res.status(404).json({ message: "Cannot find user with this id" });
                 return;
             }
             res.json(dbUserData);
@@ -43,6 +43,74 @@ const userController = {
         .then(dbUserData => res.json(dbUserData))
         .catch(err => res.status(500).json(err));
     },
+
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.userId }, body, { new: true, runValidators: true })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "Cannot find user with this id" });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.userId })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "Cannot find user with this id" });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $push: { friends: params.friendId }},
+            { new: true }
+        )
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "Cannot find user with this id" });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    deleteFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId }},
+            { new: true }
+        )
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "Cannot find user with this id" });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }
 };
 
 module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,14 +3,18 @@ const {
   getAllUsers,
   getUserById,
   createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
 } = require("../../controllers/user-controller");
 
 // GET all users
 router.route("/").get(getAllUsers).post(createUser);
 
 // GET user by ID
-router.route("/:userId").get(getUserById);
+router.route("/:userId").get(getUserById).put(updateUser).delete(deleteUser);
 
-
+//POST/DELETE friends
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
 
 module.exports = router;
